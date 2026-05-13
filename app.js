fc391c803d1038021feeae23c15bb91e77d4236f
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2026-05-13 14:17:18 -0300

    chage: "Update app.js"

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { ConfidentialClientApplication } = require("@azure/msal-node");
 const axios = require("axios");
 
 const app = express();
-const REDIRECT_URI = "http://localhost:3001/callback";
+const REDIRECT_URI = "https://enterprise-applications-monitor.onrender.com/callback";
 
 const cca = new ConfidentialClientApplication({
   auth: {
